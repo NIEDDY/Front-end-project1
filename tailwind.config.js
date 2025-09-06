cf5c22a8e93af9dd0192cbdd3a6f13cd5cb3d4c5
NIEDDY <149574944+NIEDDY@users.noreply.github.com>
2025-09-06 16:36:05 +0530

    klab assigment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // include all JS/TS/React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
